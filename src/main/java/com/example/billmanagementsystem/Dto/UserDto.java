69c0a2b93ebad3029f10026b191bca2ebb1edc99
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 18:38:40 +0530

    create UserDto.java

---
+++ src/main/java/com/example/billmanagementsystem/Dto/UserDto.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-May-21
+ *   Time: 6:38 PM
+ *   File: UserDto.java
+ */
+
+package com.example.billmanagementsystem.Dto;
+
+public class UserDto {
+}
+
