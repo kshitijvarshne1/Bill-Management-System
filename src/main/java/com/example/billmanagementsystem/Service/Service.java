24db59a93ec989a1de780afd4b157d8531c72e8d
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 18:42:45 +0530

    create Service.java

---
+++ src/main/java/com/example/billmanagementsystem/Service/Service.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-May-21
+ *   Time: 6:42 PM
+ *   File: Service.java
+ */
+
+package com.example.billmanagementsystem.Service;
+
+public class Service {
+}
+
