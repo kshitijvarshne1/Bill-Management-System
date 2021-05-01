11815f81629ad17640396c38f1c9d982419abdd0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 18:39:38 +0530

    add req fields and constructor

--- src/main/java/com/example/billmanagementsystem/Dto/UserDto.java
+++ src/main/java/com/example/billmanagementsystem/Dto/UserDto.java
@@ -7,6 +7,19 @@
 
 package com.example.billmanagementsystem.Dto;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class UserDto {
+    private String name;
+    private String email;
+    private String mobile;
+
+    public UserDto(String name, String email, String mobile) {
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+    }
 }
 
