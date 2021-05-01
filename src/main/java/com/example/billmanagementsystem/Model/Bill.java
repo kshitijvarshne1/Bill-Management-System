3e58360b89c6432a2895809feaee562f7085ffd5
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 17:50:15 +0530

    add getter, setter and default constructor

--- src/main/java/com/example/billmanagementsystem/Model/Bill.java
+++ src/main/java/com/example/billmanagementsystem/Model/Bill.java
@@ -7,8 +7,14 @@
 
 package com.example.billmanagementsystem.Model;
 
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Getter
+@Setter
+@Entity
 public class Bill {
     @Id
     private int id;
@@ -23,5 +29,9 @@ public class Bill {
         this.type = type;
         this.userId = userId;
     }
+
+    public Bill() {
+    }
+
 }
 
