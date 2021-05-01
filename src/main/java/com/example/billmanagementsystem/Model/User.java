ac22c8a27620c2ae9ca84bd15c38a36e0e7ab016
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 17:53:19 +0530

    add getter, setter and constructor

--- src/main/java/com/example/billmanagementsystem/Model/User.java
+++ src/main/java/com/example/billmanagementsystem/Model/User.java
@@ -7,9 +7,19 @@
 
 package com.example.billmanagementsystem.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
 public class User {
     private String name;
     private String email;
+    @Id
     private int id;
     private String mobile;
 
@@ -18,5 +28,9 @@ public class User {
         this.email = email;
         this.mobile = mobile;
     }
+
+    public User(){
+    }
+
 }
 
