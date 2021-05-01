4b4060de7c3d296397b512d0016386d983b6af05
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 17:49:01 +0530

    add fields and constructor

--- src/main/java/com/example/billmanagementsystem/Model/Bill.java
+++ src/main/java/com/example/billmanagementsystem/Model/Bill.java
@@ -7,6 +7,21 @@
 
 package com.example.billmanagementsystem.Model;
 
+import javax.persistence.Id;
+
 public class Bill {
+    @Id
+    private int id;
+    private String name;
+    private long amount;
+    private String type;
+    private int userId;
+
+    public Bill(String name, long amount, String type, int userId) {
+        this.name = name;
+        this.amount = amount;
+        this.type = type;
+        this.userId = userId;
+    }
 }
 
