104dc2ce65a1e7a488d958b79766d08cd1ba8d38
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 19:13:06 +0530

    final commit

--- src/main/java/com/example/billmanagementsystem/Model/User.java
+++ src/main/java/com/example/billmanagementsystem/Model/User.java
@@ -11,6 +11,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -20,6 +22,7 @@ public class User {
     private String name;
     private String email;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String mobile;
 
