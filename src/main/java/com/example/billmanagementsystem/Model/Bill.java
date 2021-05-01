5c8e8394ba80f067cf4556b844d43d930eda235b
kshitij varshney <kshitijvarshney92@gmail.com>
2021-05-01 17:40:37 +0530

    create Bill.java

---
+++ src/main/java/com/example/billmanagementsystem/Model/Bill.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 01-May-21
+ *   Time: 5:40 PM
+ *   File: Bill.java
+ */
+
+package com.example.billmanagementsystem.Model;
+
+public class Bill {
+}
+
